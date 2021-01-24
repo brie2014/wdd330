cb859aedaf1baf72ef535591d38171bf30792259
Brienna Sambrano <brie2014@byui.edu>
2021-01-23 20:50:11 -0500

    hero quiz totally reworked

--- week3/js/heroQuiz.js
+++ week3/js/heroQuiz.js
@@ -1,52 +1,155 @@
-const quiz = [
-        {question: "What is Superman's real name?", answer: "Clark Kent"},
-        {question: "Where does Superman work?", answer: "The Daily Planet"},
-        {question: "Which superhero has super tools such as the magic lasso and bullet-proof bracelets?", answer: "Wonder Woman"},
-        {question: "What is Wonder Woman's real name?", answer: "Diana Prince"},
-        {question: "What is Batman's real name?", answer: "Bruce Wayne"},
-        {question: "Batman protects which city?", answer: "Gotham City"},
-        {question: "What is Spiderman's real name", answer: "Peter Parker"},
-        {question: "What animal bit Peter Parker and gave him powers?", answer: "spider"},
-        {question: "Which superhero has an indestructible shield?", answer: "Captain America"},
-        {question: "What is Captain America's real name?", answer: "Steve Rogers"}
-    ];
-
-document.getElementById("startBtn").addEventListener("click", () => {start(quiz)});
-
-/*
-1) Display array quiz question
-2) Let user input answer, then click submit
-3) check user response against array answer
-
-*/
-function start(quiz){
-    let score = 0;
-    // main game loop
-    quiz.forEach((item) => {
-        document.getElementById("question").innerHTML = item.question;
-        const answer = item.answer;
-        document.getElementById("submitBtn").addEventListener("click", check(answer));
-    })
-
-    // end of main game loop
-    gameOver();
-   
-    //Callback functions
-
-    function check(answer){
-        const response = document.getElementById("answer").value;
-        console.log(response);
-    
-        if(response === answer){
-            document.getElementById("feedback").innerHTML ="Correct!";
-            score++;
-            } 
-            else {
-            document.getElementById("feedback").innerHTML ="Wrong! The correct answer was " + answer;
-            }
+//Functions
+function buildQuiz() {
+    const output = [];
+    questions.forEach((currentQuestion, questionNumber) => {
+        const answers = [];
+        for (letter in currentQuestion.answers) {
+            //add an HTML radio button w/ template literals and .push
+            answers.push(
+                `<label>
+                <input type="radio" name="question${questionNumber}" value="${letter}">
+                ${currentQuestion.answers[letter]}
+              </label>`
+            );
         }
-    
-    function gameOver(){
-        document.getElementById("feedback").innerHTML ="Game Over, you scored " + score + " points.";
-    }
+
+        // add question and its answers to the output w/ template literals and .push
+        output.push(
+            `<div class="slide">
+              <div class="question"> ${currentQuestion.question} </div>
+              <div class="answers"> ${answers.join("")} </div>
+            </div>`
+          );
+        }
+    );
+    //use .join to create a string from the ouput array and send it to html doc
+    quizContainer.innerHTML = output.join('');
 }
+
+function showResults(){
+
+    //select answer containers
+    const answerContainers = quizContainer.querySelectorAll('.answers');
+  
+    let numCorrect = 0;
+  
+    //check each question...
+    questions.forEach( (currentQuestion, questionNumber) => {
+      const answerContainer = answerContainers[questionNumber];
+      const selector = `input[name=question${questionNumber}]:checked`;
+      const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+  
+      // if answer is correct
+      if(userAnswer === currentQuestion.correctAnswer){
+        numCorrect++;
+        answerContainers[questionNumber].style.color = 'lightgreen';
+      }
+      // if answer is wrong or blank
+      else{
+        answerContainers[questionNumber].style.color = 'red';
+      }
+    });
+  
+    // show correct answers out of total
+    resultsContainer.innerHTML = `You got ${numCorrect} out of ${questions.length} correct`;
+  }
+
+//Variables
+const quizContainer = document.getElementById('quiz');
+const resultsContainer = document.getElementById('results');
+const questions = [
+    {
+        question: "Where does Superman work?",
+        answers: {
+            a: "Stark Industries",
+            b: "The Daily Planet",
+            c: "Gotham Police Department"
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "Which superhero has super tools such as the magic lasso and bullet-proof bracelets?",
+        answers: {
+            a: "Superman",
+            b: "Bat Girl",
+            c: "Wonder Woman"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "Batman protects which city?",
+        answers: {
+            a: "Gotham City",
+            b: "Metropolis",
+            c: "New York City"
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "What animal bit Peter Parker and gave him powers?",
+        answers: {
+            a: "spider",
+            b: "bear",
+            c: "ant"
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "Which superhero has an indestructible shield?",
+        answers: {
+            a: "The Flash",
+            b: "Green Lantern",
+            c: "Captain America"
+        },
+        correctAnswer: "c"
+    },
+];
+
+//Create quiz
+buildQuiz();
+
+
+//Variables for slides
+const previousButton = document.getElementById("previousBtn");
+const nextButton = document.getElementById("nextBtn");
+const slides = document.querySelectorAll(".slide");
+const submitButton = document.getElementById("submitBtn");
+let currentSlide = 0;
+
+//Functions for slides
+function showSlide(n) {
+    slides[currentSlide].classList.remove('active-slide');
+    slides[n].classList.add('active-slide');
+    currentSlide = n;
+    if(currentSlide === 0){
+      previousButton.style.display = 'none';
+    }
+    else{
+      previousButton.style.display = 'inline-block';
+    }
+    if(currentSlide === slides.length-1){
+      nextButton.style.display = 'none';
+      submitButton.style.display = 'inline-block';
+    }
+    else{
+      nextButton.style.display = 'inline-block';
+      submitButton.style.display = 'none';
+    }
+  }
+
+  function showNextSlide() {
+    showSlide(currentSlide + 1);
+  }
+  
+  function showPreviousSlide() {
+    showSlide(currentSlide - 1);
+  }
+
+//Make first question visible
+  showSlide(currentSlide);
+
+
+//Functions run on button clicks
+submitButton.addEventListener('click', showResults);
+previousButton.addEventListener("click", showPreviousSlide);
+nextButton.addEventListener("click", showNextSlide);
--- week3/js/heroQuizSandbox.js
+++ week3/js/heroQuizSandbox.js
@@ -48,3 +48,61 @@ function start(quiz){
     }
 }
 
+
+
+
+
+
+
+const quiz = [
+    {question: "What is Superman's real name?", answer: "Clark Kent"},
+    {question: "Where does Superman work?", answer: "The Daily Planet"},
+    {question: "Which superhero has super tools such as the magic lasso and bullet-proof bracelets?", answer: "Wonder Woman"},
+    {question: "What is Wonder Woman's real name?", answer: "Diana Prince"},
+    {question: "What is Batman's real name?", answer: "Bruce Wayne"},
+    {question: "Batman protects which city?", answer: "Gotham City"},
+    {question: "What is Spiderman's real name", answer: "Peter Parker"},
+    {question: "What animal bit Peter Parker and gave him powers?", answer: "spider"},
+    {question: "Which superhero has an indestructible shield?", answer: "Captain America"},
+    {question: "What is Captain America's real name?", answer: "Steve Rogers"}
+];
+
+document.getElementById("startBtn").addEventListener("click", () => {start(quiz)});
+
+/*
+1) Display array quiz question
+2) Let user input answer, then click submit
+3) check user response against array answer
+
+*/
+function start(quiz){
+let score = 0;
+// main game loop
+quiz.forEach((item) => {
+    document.getElementById("question").innerHTML = item.question;
+    const answer = item.answer;
+    document.getElementById("submitBtn").addEventListener("click", check(answer));
+})
+
+// end of main game loop
+gameOver();
+
+//Callback functions
+
+function check(answer){
+    const response = document.getElementById("answer").value;
+    console.log(response);
+
+    if(response === answer){
+        document.getElementById("feedback").innerHTML ="Correct!";
+        score++;
+        } 
+        else {
+        document.getElementById("feedback").innerHTML ="Wrong! The correct answer was " + answer;
+        }
+    }
+
+function gameOver(){
+    document.getElementById("feedback").innerHTML ="Game Over, you scored " + score + " points.";
+}
+}
